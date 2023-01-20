c69c47af7d6e7da7e08e8d303a0f9e4f0d819498
bkraske <71412733+bkraske@users.noreply.github.com>
2023-01-20 16:34:40 -0700

    Progress

--- src/solver.jl
+++ src/solver.jl
@@ -10,6 +10,32 @@ function PBVISolver2(; max_iterations::Int64=10, ϵ::Float64=0.01, verbose::Bool
     return PBVISolver2(max_iterations, ϵ, verbose)
 end
 
+struct PBVI_cache{S,A}
+    spomdp::SparseTabularPOMDP
+    ordered_states::Vector{S}
+    ordered_actions::Vector{A}
+    terminals::Union{Vector{Int64},Vector{Union{}}}
+    not_terminals::Vector{Int64}
+    b::Vector{Float64}
+    Γa::Vector{Vector{Float64}}
+    Γao::Vector{Vector{Float64}}
+end
+
+function PBVI_cache(pomdp::POMDP)
+    spomdp = SparseTabularPOMDP(pomdp)
+    ordered_s = ordered_states(pomdp)
+    ordered_a = ordered_actions(pomdp)
+    terminals = [stateindex(pomdp, s) for s in ordered_s if isterminal(pomdp, s)]
+    not_terminals = [stateindex(pomdp, s) for s in ordered_s if !isterminal(pomdp, s)]
+    b = Vector{Float64}(undef,length(states(pomdp)))
+    Γa = Vector{Vector{Float64}}(undef, length(ordered_a))
+    Γao = Vector{Vector{Float64}}(undef, length(ordered_observations(pomdp)))
+    return PBVI_cache(spomdp,ordered_s,ordered_a,
+                      terminals,not_terminals,b,Γa,Γao)
+end
+
+
+
 """
     AlphaVec
 Pair of alpha vector and corresponding action.
@@ -52,7 +78,7 @@ end
 function belief_norm(pomdp, b, b′, terminals, not_terminals)
     if sum(view(b′,not_terminals)) != 0.0
         if !isempty(terminals)
-            @views b′[not_terminals] ./= (sum(b′[not_terminals]) / (1. - sum(b[terminals]) - sum(b′[terminals])))
+            @views b′[not_terminals] ./= (sum(b′[not_terminals]) / (1.0 - sum(b[terminals]) - sum(b′[terminals])))
             @views b′[terminals] += b[terminals]
         else
             @views b′[not_terminals] ./= sum(b′[not_terminals])
@@ -65,35 +91,32 @@ function belief_norm(pomdp, b, b′, terminals, not_terminals)
 end
 
 # Backups belief with α vector maximizing dot product of itself with belief b
-function backup_belief(pomdp::POMDP, Γ, b, spomdp)
-    S = ordered_states(pomdp)
-    A = ordered_actions(pomdp)
-    # O = ordered_observations(pomdp)
-    γ = discount(pomdp)
-    # r = StateActionReward(pomdp)
+function backup_belief(pomdp::POMDP, cache::PBVI_cache, Γ, b) #ADD WITNESSES?
+    S = cache.ordered_states
+    A = cache.ordered_actions
+    γ = cache.spomdp.discount
 
-    Γa = Vector{Vector{Float64}}(undef, length(A))
+    Γa = cache.Γa #Vector{Vector{Float64}}(undef, length(A))
 
-    terminals = spomdp.terminal_states
-    not_terminals = deleteat!(collect(1:length(S)),terminals)
-    # not_terminals = [stateindex(pomdp, s) for s in S if !isterminal(pomdp, s)]
-    # terminals = [stateindex(pomdp, s) for s in S if isterminal(pomdp, s)]
+    terminals = cache.terminals
+    not_terminals = cache.not_terminals
+
+    trans_probs = zeros(length(S))
     for a in A
-        a_ind = actionindex(pomdp,a)
-        O1 = spomdp.O[a_ind]
-        T = spomdp.T[a_ind]
-        O_len = length(O1[1,:])
-        Γao = Vector{Vector{Float64}}(undef,O_len)
-        # @show [pdf(transition(pomdp, S[is], a), sp) * b.b[is] for sp in S, is in not_terminals]
-        # @show trans_probs = dropdims(sum([pdf(transition(pomdp, S[is], a), sp) * b.b[is] for sp in S, is in not_terminals], dims=2), dims=2)
-        trans_probs = zeros(length(S))
+        a_ind = actionindex(pomdp, a)
+        O1 = cache.spomdp.O[a_ind]
+        T = cache.spomdp.T[a_ind]
+        O_len = length(O1[1, :])
+
+        Γao = cache.Γao #Vector{Vector{Float64}}(undef, O_len)
+
+        trans_probs .= 0.0
         for is ∈ not_terminals
             for sp ∈ S
-                sp_i = stateindex(pomdp,sp)
-                trans_probs[sp_i] += b.b[is]*T[is,sp_i]
+                sp_i = stateindex(pomdp, sp)
+                trans_probs[sp_i] += b.b[is] * T[is, sp_i]
             end
         end
-        # @show trans_probs
 
         if !isempty(terminals)
             trans_probs[terminals] .+= b.b[terminals]
@@ -101,34 +124,25 @@ function backup_belief(pomdp::POMDP, Γ, b, spomdp)
 
         for o_i in 1:O_len
             # update beliefs
-            # obs_probs = pdf.(map(sp -> observation(pomdp, a, sp), S), [o])
-            # @show pdf.(map(sp -> observation(pomdp, a, sp), S), [ordered_observations(pomdp)[o_i]])
-            obs_probs = O1[:,o_i]
-            # @show obs_probs
-            b′ = Vector(obs_probs .* trans_probs)
+            @views obs_probs = O1[:,o_i]
+            b′ = cache.b
+            b′ .= obs_probs .* trans_probs
 
             if sum(b′) > 0.0
-                b′ = DiscreteBelief(pomdp, b.state_list, belief_norm(pomdp, b.b, b′, terminals, not_terminals))
-            else
-                b′ = DiscreteBelief(pomdp, b.state_list, zeros(length(S)))
+                b′ = belief_norm(pomdp, b.b, b′, terminals, not_terminals)
             end
 
             # extract optimal alpha vector at resulting belief
-            Γao[o_i] = max_alpha_val(Γ, b′.b)
+            Γao[o_i] = max_alpha_val(Γ, b′)
         end
 
-        # construct new alpha vectors
-        # Γa[actionindex(pomdp, a)] = [r(s, a) + (!isterminal(pomdp, s) ? (γ * sum(pdf(transition(pomdp, s, a), sp) * pdf(observation(pomdp, s, a, sp), o) * Γao[i][j]
-        #                                                                          for (j, sp) in enumerate(S), (i, o) in enumerate(O))) : 0.0)
-        #                              for s in S]
-        
         Γa[a_ind] = zeros(length(S))
         for s_i in 1:length(S)
-            Γa[a_ind][s_i] = spomdp.R[s_i,a_ind] 
+            Γa[a_ind][s_i] = cache.spomdp.R[s_i, a_ind]
             for sp_i in 1:length(S)
                 for o_i in 1:O_len
                     if s_i ∉ terminals
-                        Γa[a_ind][s_i] += γ*T[s_i,sp_i]*O1[sp_i,o_i]*Γao[o_i][sp_i]
+                        Γa[a_ind][s_i] += γ * T[s_i, sp_i] * O1[sp_i, o_i] * Γao[o_i][sp_i]
                     end
                 end
             end
@@ -143,12 +157,11 @@ function backup_belief(pomdp::POMDP, Γ, b, spomdp)
 end
 
 # Iteratively improves α vectors until the gap between steps is lesser than ϵ
-function improve(pomdp, B, Γ, solver)
+function improve(pomdp, cache, B, Γ, solver)
     alphavecs = nothing
-    spomdp = SparseTabularPOMDP(pomdp)
     while true
         Γold = Γ
-        alphavecs = [backup_belief(pomdp, Γold, b, spomdp) for b in B]
+        alphavecs = [backup_belief(pomdp, cache, Γold, b) for b in B]
         Γ = [alphavec.alpha for alphavec in alphavecs]
         prec = max([sum(abs.(dot(α1, b.b) .- dot(α2, b.b))) for (α1, α2, b) in zip(Γold, Γ, B)]...)
         if solver.verbose
@@ -161,21 +174,35 @@ function improve(pomdp, B, Γ, solver)
 end
 
 # Returns all possible, not yet visited successors of current belief b
-function successors(pomdp, b, Bs)
-    S = ordered_states(pomdp)
-    not_terminals = [stateindex(pomdp, s) for s in S if !isterminal(pomdp, s)]
-    terminals = [stateindex(pomdp, s) for s in S if isterminal(pomdp, s)]
+function successors(pomdp, cache, b, Bs)
+    S = cache.ordered_states
+    not_terminals = cache.not_terminals
+    terminals = cache.terminals
     succs = []
 
+    trans_probs = zeros(length(S))
     for a in actions(pomdp)
-        trans_probs = dropdims(sum([pdf(transition(pomdp, S[is], a), sp) * b[is] for sp in S, is in not_terminals], dims=2), dims=2)
+        a_ind = actionindex(pomdp, a)
+        O1 = cache.spomdp.O[a_ind]
+        T = cache.spomdp.T[a_ind]
+        O_len = length(O1[1, :])
+
+        trans_probs .= 0.0
+        for is ∈ not_terminals
+            for sp ∈ S
+                sp_i = stateindex(pomdp, sp)
+                trans_probs[sp_i] += b[is] * T[is, sp_i]
+            end
+        end
+
+        # trans_probs = dropdims(sum([pdf(transition(pomdp, S[is], a), sp) * b[is] for sp in S, is in not_terminals], dims=2), dims=2)
         if !isempty(terminals)
             trans_probs[terminals] .+= b[terminals]
         end
 
-        for o in observations(pomdp)
+        for o_i in 1:O_len #observations(pomdp)
             #update belief
-            obs_probs = pdf.(map(sp -> observation(pomdp, a, sp), S), [o])
+            @views obs_probs = O1[:,o_i]
             b′ = obs_probs .* trans_probs
 
 
@@ -200,10 +227,10 @@ end
 
 # Expands the belief space with the most distant belief vector
 # Returns new belief space, set of belifs and early termination flag
-function expand(pomdp, B, Bs)
+function expand(pomdp, cache, B, Bs)
     B_new = copy(B)
     for b in B
-        succs = successors(pomdp, b.b, Bs)
+        succs = successors(pomdp, cache, b.b, Bs)
         if length(succs) > 0
             b′ = succs[argmax([succ_dist(pomdp, bp, B) for bp in succs])]
             push!(B_new, DiscreteBelief(pomdp, b′))
@@ -236,14 +263,14 @@ function solve(solver::PBVISolver2, pomdp::POMDP)
     if solver.verbose
         println("Running PBVI solver on $(typeof(pomdp)) problem with following settings:\n    max_iterations = $(solver.max_iterations), ϵ = $(solver.ϵ), verbose = $(solver.verbose)\n+----------------------------------------------------------+")
     end
-
+    cache = PBVI_cache(pomdp)
     # original code should run until V converges to V*, this yet needs to be implemented
     # for example as: while max(@. abs(newV - oldV)...) > solver.ϵ
     # However this probably would not work, as newV and oldV have different number of elements (arrays of alphas)
     alphavecs = nothing
     for i in 1:solver.max_iterations
-        Γ, alphavecs = improve(pomdp, B, Γ, solver)
-        B, Bs, early_term = expand(pomdp, B, Bs)
+        Γ, alphavecs = improve(pomdp, cache, B, Γ, solver)
+        B, Bs, early_term = expand(pomdp, cache, B, Bs)
         if solver.verbose
             println("Iteration $(i) executed, belief set contains $(length(Bs)) belief vectors.")
         end

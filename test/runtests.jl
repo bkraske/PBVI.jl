0b507b59448ef0554c921f26b20b353c45375804
WhiffleFish <tylertronics@gmail.com>
2023-02-07 17:57:49 -0700

    tiger test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
 using Test
 using PBVI
 using SparseArrays
+using POMDPModels
+using POMDPs
+
+@testset "tiger" begin
+    pomdp = TigerPOMDP()
+    sol = PBVISolver(max_iter=10, verbose=true)
+    alpha_policy = solve(sol, pomdp)
+    v0 = value(alpha_policy, initialstate(pomdp))
+    @test isapprox(v0, 19.2; atol=0.1)
+end
 
 include("sparse_stuff.jl")
